ffc1396b6445406482a016d138f67e2a4f9717d8
Primalpat- <r.patrickwilliams@gmail.com>
2020-02-23 00:50:04 -0500

    Starting on Home Screen!

---
+++ NotesOfOrder/NotesOfOrder.Web/Controllers/NavigatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NotesOfOrder.Web.Controllers
+{
+    public class NavigatorController : Controller
+    {
+        private readonly ILogger<NavigatorController> _logger;
+
+        public NavigatorController(ILogger<NavigatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
